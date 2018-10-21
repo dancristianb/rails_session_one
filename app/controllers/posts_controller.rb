9419d295db4da1cb4641ceaf56373efb67101ba1
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-21 15:51:27 +0300

    switch indentation and fix post create endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def show
-    end
+  def show; end
 
-    def create
-        @post = Post.create(post_params)
-        render :show, status: :created
-    end
+  def create
+    @post = Post.create(post_params)
+    render :show, status: :created
+  end
 
-    private
+  private
 
-    def post_params
-        params.require(:post).permit(:title, :desc, :active)
-    end
+  def post_params
+    params.require(:posts).permit(:title, :desc, :active)
+  end
 end
